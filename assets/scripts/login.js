605fc607d4b869bce0aa89f24dded1607f0397ea
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2025-12-06 00:01:27 +0100

    Refactor authentication pages: replace login functionality with signup, update styles, and remove unused CSS

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,13 +1,3 @@
- function startAnimation() {
-      const logo = document.getElementById("splash_logo");
-      const mainContent = document.getElementById("main_content");
-      const splash = document.getElementById("splash");
-
-      mainContent.style.opacity = "1";
-      setTimeout(() => {
-        logo.classList.add("animate");
-      }, 100);
-      setTimeout(() => {
-        splash.style.display = "none";
-      }, 1000);
-    }
+function goToSignup() {
+  window.location.href="sign_up.html";
+}
