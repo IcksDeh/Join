1c027bfb02e097b8008a62cae267123b9f88f8ad
IcksDeh <diagnose-neongruen@gmx.de>
2025-12-08 20:20:18 +0100

    refactoring links, added sidebar, navbar to boardside, contact side

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function init(){
-    // loadSidebar();
+    loadSidebar();
     loadNavbar();
     loadSummary();
 }
--- templates/sidebar_navbar_template.js
+++ templates/sidebar_navbar_template.js
@@ -1,32 +1,29 @@
 function sidebarLoginTemplate(){
     return `
-             <img class = "logo_invert" src="./assets/img/logo_version2.svg" alt="Join Logo">
+            <img class = "logo_invert" src="./assets/img/logo_version2.svg" alt="Join Logo">
             <div class="order_selection_privacy_legal_sidebar">
                 <div class="sidebar_selection">
-                    <div class="order_icon_selection ">
+                    <a class="order_icon_selection" href="./summary.html">
                         <img class="icon_sidebar" src="./assets/img/summary_noneactivated.svg" alt="">
-                        <a href="./summary.html">Summary</a>
-                    </div>
-                    <div class="order_icon_selection">
+                        <p>Summary</p>
+                    </a>
+                    <a class="order_icon_selection" href="./add_task.html">
                         <img class="icon_sidebar" src="./assets/img/add_task_icon.svg" alt="">
-                        <a href="./add_task.html">Add Task</a>
-                    </div>
-                    <div class="order_icon_selection">
+                        <p>Add Task</p>
+
+                    </a>
+                    <a class="order_icon_selection" href="./board.html">
                         <img class="icon_sidebar" src="./assets/img/board_icon.svg" alt="">
-                        <a href="./board.html">Board</a>
-                    </div>
-                    <div class="order_icon_selection">
+                        <p>Board</p>
+                    </a>
+                    <a class="order_icon_selection" href="./contacts.html">
                         <img class="icon_sidebar" src="./assets/img/contacts_icon.svg" alt="">
-                        <a href="./contacts.html">Contacts</a>
-                    </div>
+                        <p>Contacts</p>
+                    </a>
                 </div>   
                 <div class="sidebar_privacy_legal">
-                    <div class="design_privacy_legal">
-                        <a href="./privacy_policy.html">Privacy Policy</a>
-                    </div>
-                    <div class="design_privacy_legal">
-                        <a href="./legal_notice.html">Legal Notice</a>
-                    </div>
+                    <a class="design_privacy_legal" href="./privacy_policy.html">Privacy Policy</a>
+                    <a class="design_privacy_legal" href="./legal_notice.html">Legal Notice</a>
                 </div>
             </div> 
     `
@@ -36,19 +33,13 @@ function sidebarLogOffTemplate(){
     return `
         <img class = "logo_invert" src="./assets/img/logo_version2.svg" alt="Join Logo">
             <div class="order_selection_privacy_legal_sidebar">
-                <div class="style_login_icon_text">
+                <a class="style_login_icon_text" href="./index.html">
                     <img src="./assets/img/log_in.svg" alt="Login">
                     <p class="p_text">Log In</p>
-                   
-                </div> 
-
+                </a>
                 <div class="sidebar_privacy_legal">
-                    <div class="design_privacy_legal">
-                        <a href="#">Privacy Policy</a>
-                    </div>
-                    <div class="design_privacy_legal">
-                        <a href="#">Legal Notice</a>
-                    </div>
+                    <a class="design_privacy_legal" href="./privacy_policy.html">Privacy Policy</a>
+                    <a class="design_privacy_legal" href="./legal_notice.html">Legal Notice</a>
                 </div>
             </div>
     `
@@ -59,7 +50,7 @@ function navbarTemplate(){
             <div class = "content_wrapper order_content_navbar">    
                 <p class="text_navbar">Kanban Projekt Management Tool</p>
                 <div class="help_user_icon">
-                    <img class="help_icon" src="./assets/img/help.svg" alt="Help">
+                         <a href="./help.html"><img class="help_icon" src="./assets/img/help.svg" alt="Help"></a>
                     <div class="user_circle">AM</div>
                 </div>
             </div>
