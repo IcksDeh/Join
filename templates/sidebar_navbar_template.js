c3946c8d3c6c94b2bf6ba6dbb5910b6ac890d853
Karina Klages <karinaklages@web.de>
2025-12-09 09:42:31 +0100

    structure updated

--- templates/sidebar_navbar_template.js
+++ templates/sidebar_navbar_template.js
@@ -1,60 +1,58 @@
 function sidebarLoginTemplate(){
     return `
-            <img class = "logo_invert" src="./assets/img/logo_version2.svg" alt="Join Logo">
-            <div class="order_selection_privacy_legal_sidebar">
-                <div class="sidebar_selection">
-                    <a class="order_icon_selection" href="./summary.html">
-                        <img class="icon_sidebar" src="./assets/img/summary_noneactivated.svg" alt="">
-                        <p>Summary</p>
-                    </a>
-                    <a class="order_icon_selection" href="./add_task.html">
-                        <img class="icon_sidebar" src="./assets/img/add_task_icon.svg" alt="">
-                        <p>Add Task</p>
+        <img class = "logo_invert" src="./assets/img/logo_version2.svg" alt="Join Logo">
+        <div class="order_selection_privacy_legal_sidebar">
+            <div class="sidebar_selection">
+                <a class="order_icon_selection" href="./summary.html">
+                    <img class="icon_sidebar" src="./assets/img/summary_noneactivated.svg" alt="">
+                    <p>Summary</p>
+                </a>
+                <a class="order_icon_selection" href="./add_task.html">
+                    <img class="icon_sidebar" src="./assets/img/add_task_icon.svg" alt="">
+                    <p>Add Task</p>
 
-                    </a>
-                    <a class="order_icon_selection" href="./board.html">
-                        <img class="icon_sidebar" src="./assets/img/board_icon.svg" alt="">
-                        <p>Board</p>
-                    </a>
-                    <a class="order_icon_selection" href="./contacts.html">
-                        <img class="icon_sidebar" src="./assets/img/contacts_icon.svg" alt="">
-                        <p>Contacts</p>
-                    </a>
-                </div>   
-                <div class="sidebar_privacy_legal">
-                    <a class="design_privacy_legal" href="./privacy_policy.html">Privacy Policy</a>
-                    <a class="design_privacy_legal" href="./legal_notice.html">Legal Notice</a>
-                </div>
-            </div> 
+                </a>
+                <a class="order_icon_selection" href="./board.html">
+                    <img class="icon_sidebar" src="./assets/img/board_icon.svg" alt="">
+                    <p>Board</p>
+                </a>
+                <a class="order_icon_selection" href="./contacts.html">
+                    <img class="icon_sidebar" src="./assets/img/contacts_icon.svg" alt="">
+                    <p>Contacts</p>
+                </a>
+            </div>   
+            <div class="sidebar_privacy_legal">
+                <a class="design_privacy_legal" href="./privacy_policy.html">Privacy Policy</a>
+                <a class="design_privacy_legal" href="./legal_notice.html">Legal Notice</a>
+            </div>
+        </div> 
     `
 }
 
 function sidebarLogOffTemplate(){
     return `
         <img class = "logo_invert" src="./assets/img/logo_version2.svg" alt="Join Logo">
-            <div class="order_selection_privacy_legal_sidebar">
-                <a class="style_login_icon_text" href="./index.html">
-                    <img src="./assets/img/log_in.svg" alt="Login">
-                    <p class="p_text">Log In</p>
-                </a>
-                <div class="sidebar_privacy_legal">
-                    <a class="design_privacy_legal" href="./privacy_policy.html">Privacy Policy</a>
-                    <a class="design_privacy_legal" href="./legal_notice.html">Legal Notice</a>
-                </div>
+        <div class="order_selection_privacy_legal_sidebar">
+            <a class="style_login_icon_text" href="./index.html">
+                <img src="./assets/img/log_in.svg" alt="Login">
+                <p class="p_text">Log In</p>
+            </a>
+            <div class="sidebar_privacy_legal">
+                <a class="design_privacy_legal" href="./privacy_policy.html">Privacy Policy</a>
+                <a class="design_privacy_legal" href="./legal_notice.html">Legal Notice</a>
             </div>
+        </div>
     `
 }
 
 function navbarTemplate(){
     return `
-            <div class = "content_wrapper order_content_navbar">    
-                <p class="text_navbar">Kanban Projekt Management Tool</p>
-                <div class="help_user_icon">
-                         <a href="./help.html"><img class="help_icon" src="./assets/img/help.svg" alt="Help"></a>
-                    <div class="user_circle">AM</div>
-                </div>
+        <div class = "content_wrapper order_content_navbar">    
+            <p class="text_navbar">Kanban Projekt Management Tool</p>
+            <div class="help_user_icon">
+                <a href="./help.html"><img class="help_icon" src="./assets/img/help.svg" alt="Help"></a>
+            <div class="user_circle">AM</div>
             </div>
-
+        </div>
     `
-
 }
--- templates/summary_content.js
+++ templates/summary_content.js
@@ -1,54 +1,54 @@
 function summaryContentTemplate(){
     return `
         <div>
-                        <section class="order_todo_done_box margin_left">
-                            <div class="style_todo_done_box style_boxes">
-                                <img class="img_summary_edit_check_icon" src="./assets/img/summary_edit_icon.svg" alt="edit">
-                                <div class="order_number_text">
-                                    <p class="summary_number">1</p>
-                                    <p>To-do</p>
-                                </div>
-                            </div>
-                            <div class="style_todo_done_box style_boxes">
-                                <img class="img_summary_edit_check_icon" src="./assets/img/summary_check_icon.svg" alt="check">
-                                <div class="order_number_text">
-                                    <p class="summary_number">2</p>
-                                    <p>Done</p>
-                                </div>
-                            </div>
-                        </section>
-                        <section class="box_prio_tickets  margin_left style_boxes">
-                            <div class="counter_prio_tickets">
-                                <img src="./assets/img/summary_prio_icon.svg" alt="prio">
-                                <div class="order_number_text">
-                                    <p class="summary_number">3</p>
-                                    <p>Urgent</p>
-                                </div>
-                            </div>
-                            <div class="divider_prio_box "></div>
-                            <div class="deadline_information">
-                                <p class="date_of_deadline">October 16, 2022</p>
-                                <p>Upcoming Deadline</p>
-                            </div>
-                        </section>
-                        <section class="order_ticket_box_below margin_left">
-                                <div class="order_number_text style_boxes boxes_last_row">
-                                    <p class="summary_number">4</p>
-                                    <p class="style_text">Tasks in <br> Board</p>
-                                </div>
-                                <div class="order_number_text style_boxes boxes_last_row">
-                                    <p class="summary_number">5</p>
-                                    <p class="style_text">Tasks in <br> Progress</p>
-                                </div>
-                                <div class="order_number_text style_boxes boxes_last_row">
-                                    <p class="summary_number">6</p>
-                                    <p class="style_text">Awaiting <br> Feedback</p>
-                                </div>
-                        </section>
+            <section class="order_todo_done_box margin_left">
+                <div class="style_todo_done_box style_boxes">
+                    <img class="img_summary_edit_check_icon" src="./assets/img/summary_edit_icon.svg" alt="edit">
+                    <div class="order_number_text">
+                        <p class="summary_number">1</p>
+                        <p>To-do</p>
                     </div>
-                    <div class="order_greeting_username">
-                        <p class="style_greeting_text">Good morning,</p>
-                        <h1 class="style_username">Sofia Müller</h1>
+                </div>
+                <div class="style_todo_done_box style_boxes">
+                    <img class="img_summary_edit_check_icon" src="./assets/img/summary_check_icon.svg" alt="check">
+                    <div class="order_number_text">
+                        <p class="summary_number">2</p>
+                        <p>Done</p>
                     </div>
+                </div>
+            </section>
+            <section class="box_prio_tickets  margin_left style_boxes">
+                <div class="counter_prio_tickets">
+                    <img src="./assets/img/summary_prio_icon.svg" alt="prio">
+                    <div class="order_number_text">
+                        <p class="summary_number">3</p>
+                        <p>Urgent</p>
+                    </div>
+                </div>
+                <div class="divider_prio_box "></div>
+                <div class="deadline_information">
+                    <p class="date_of_deadline">October 16, 2022</p>
+                    <p>Upcoming Deadline</p>
+                </div>
+            </section>
+            <section class="order_ticket_box_below margin_left">
+                    <div class="order_number_text style_boxes boxes_last_row">
+                        <p class="summary_number">4</p>
+                        <p class="style_text">Tasks in <br> Board</p>
+                    </div>
+                    <div class="order_number_text style_boxes boxes_last_row">
+                        <p class="summary_number">5</p>
+                        <p class="style_text">Tasks in <br> Progress</p>
+                    </div>
+                    <div class="order_number_text style_boxes boxes_last_row">
+                        <p class="summary_number">6</p>
+                        <p class="style_text">Awaiting <br> Feedback</p>
+                    </div>
+            </section>
+        </div>
+        <div class="order_greeting_username">
+            <p class="style_greeting_text">Good morning,</p>
+            <h1 class="style_username">Sofia Müller</h1>
+        </div>
     `
 }
