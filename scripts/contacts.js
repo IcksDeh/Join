301b30c9a234e2e6744792165d33f5d2481fcee8
Schuhoch2 <githubmain@tgsweb.de>
2025-12-17 22:19:53 +0100

    added functions for template-using

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,17 +1,17 @@
-document.getElementById('createContactBtn').addEventListener("click", async function(event){
+document.getElementById('createContactBtn').addEventListener("click", async function (event) {
     event.preventDefault();
     await getContactData();
-    })
+})
 
-function getElementsContacts(){
-   return { 
-    name : document.getElementById('id_contact_name'),
-    email: document.getElementById('id_contact_email'),
-    phonenumber: document.getElementById('id_contact_phone'),
-  };
+function getElementsContacts() {
+    return {
+        name: document.getElementById('id_contact_name'),
+        email: document.getElementById('id_contact_email'),
+        phonenumber: document.getElementById('id_contact_phone'),
+    };
 }
 
-async function getContactData(){
+async function getContactData() {
     const elements = getElementsContacts();
     let contactName = elements.name.value;
     let contactEmail = elements.email.value;
@@ -22,9 +22,35 @@ async function getContactData(){
 
 async function switchContactsData(contactName, contactEmail, contactPhonenumber) {
     let contactData = {
-        "name" : contactName,
-        "eMail" : contactEmail,
-        "phoneNumber" : contactPhonenumber,
+        "name": contactName,
+        "eMail": contactEmail,
+        "phoneNumber": contactPhonenumber,
     }
-    await putToStorage ("contacts", contactData)
+    await putToStorage("contacts", contactData)
+}
+
+function contactActive(element) {
+    const isActive = element.classList.contains("active-contact");
+    const cInfo = document.getElementById('contact-info')
+    const contacts = document.getElementsByClassName("contact-person");
+    for (let i = 0; i < contacts.length; i++) {
+        contacts[i].classList.remove("active-contact");
+    }
+    if (isActive) {
+        cInfo.innerHTML = contactHeadline();
+        return;
+    }
+    element.classList.add("active-contact");
+
+    const index = Number(element.id.replace("contact-", ""));
+    const selectedContact = contact[index];
+    cInfo.innerHTML = contactHeadline() + contactInfo(selectedContact) + moreContactInfo(selectedContact)
+}
+
+function cList() {
+    const cList = document.getElementById('contact-list')
+    for (let index = 0; index < contact.length; index++) {
+        const element = contact[index];
+        cList.innerHTML += loadContactList(element, index)
+    }
 }
