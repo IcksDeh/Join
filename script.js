bc689b0d769dc5bd082da2de27436470a00f8c4a
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-01-06 20:58:39 +0100

    feat: add null check for icon in highlightActiveLink function

--- script.js
+++ script.js
@@ -245,23 +245,26 @@ function highlightActiveLink() {
 
     links.forEach(link => {
         const linkPath = link.getAttribute('href').replace('./', '');
-        const icon = link.querySelector('.icon_sidebar');
+        const icon = link.querySelector('.icon_sidebar'); 
 
         if (currentPath.includes(linkPath)) {
             link.classList.add('active');
-            if (linkPath === 'summary.html') {
-                icon.src = './assets/img/navbar_summary_white_mobile.svg';
-            } else if (linkPath === 'add_task.html') {
-                icon.src = './assets/img/add_task_white.svg';
-            } else if (linkPath === 'board.html') {
-                icon.src = './assets/img/navbar_board_white_mobile.svg';
-            } else if (linkPath === 'contacts.html') {
-                icon.src = './assets/img/navbar_contacts_white_mobile.svg';
+            
+        
+            if (icon) { 
+                if (linkPath === 'summary.html') {
+                    icon.src = './assets/img/navbar_summary_white_mobile.svg';
+                } else if (linkPath === 'add_task.html') {
+                    icon.src = './assets/img/add_task_white.svg';
+                } else if (linkPath === 'board.html') {
+                    icon.src = './assets/img/navbar_board_white_mobile.svg';
+                } else if (linkPath === 'contacts.html') {
+                    icon.src = './assets/img/navbar_contacts_white_mobile.svg';
+                }
             }
         }
     });
 }
-
 
 /**
  *  Displays a toast notification for a short duration.
