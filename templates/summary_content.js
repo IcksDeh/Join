a101cd59f5e990339d680fdb27fb040c440ce99c
IcksDeh <diagnose-neongruen@gmx.de>
2026-01-12 20:54:07 +0100

    summary content is loaded dynamically except dueDate

--- templates/summary_content.js
+++ templates/summary_content.js
@@ -5,14 +5,14 @@ function summaryContentTemplate(userName, greeting) {
                 <div class="style_todo_done_box style_boxes style_done_box_hover" onclick="goToBoardPage()">
                     <img class="img_summary_edit_check_icon" src="./assets/img/summary_edit_icon.svg" alt="Edit Icon">
                     <div class="order_number_text">
-                        <p class="summary_number">1</p>
+                        <p id="id_summary_todo" class="summary_number"></p>
                         <p class="style_text">To Do</p>
                     </div>
                 </div>
                 <div class="style_todo_done_box style_boxes style_todo_box_hover" onclick="goToBoardPage()">
                     <img class="img_summary_edit_check_icon" src="./assets/img/summary_check_icon.svg" alt="Check Icon">
                     <div class="order_number_text">
-                        <p class="summary_number">2</p>
+                        <p id="id_summary_done" class="summary_number"></p>
                         <p class="style_text">Done</p>
                     </div>
                 </div>
@@ -22,28 +22,28 @@ function summaryContentTemplate(userName, greeting) {
                  <div class="counter_prio_tickets">
                     <img class="board_priority_btn" src="./assets/img/summary_prio_icon.svg" alt="Priority Icon">
                     <div class="order_number_text">
-                        <p class="summary_number">3</p>
+                        <p id="id_summary_urgent" class="summary_number"></p>
                         <p class="style_prio_area">Urgent</p>
                     </div>
                 </div>
                 <div class="divider_prio_box"></div>
                 <div class="deadline_information">
-                    <p class="date_of_deadline">October 16, 2022</p>
+                    <p id="id_summary_dueDate" class="date_of_deadline"></p>
                     <p class="style_prio_area">Upcoming Deadline</p>
                 </div>
             </section>
 
             <section class="order_ticket_box_below margin_left">
                  <div class="order_number_text style_boxes boxes_last_row" onclick="goToBoardPage()">
-                        <p class="summary_number">4</p>
+                        <p id="id_summary_allTasks" class="summary_number"></p>
                         <p class="style_text">Tasks in <br> Board</p>
                     </div>
                     <div class="order_number_text style_boxes boxes_last_row" onclick="goToBoardPage()">
-                        <p class="summary_number">5</p>
+                        <p id="id_summary_inProgress" class="summary_number"></p>
                         <p class="style_text">Tasks in <br> Progress</p>
                     </div>
                     <div class="order_number_text style_boxes boxes_last_row" onclick="goToBoardPage()">
-                        <p class="summary_number">6</p>
+                        <p id="id_summary_awaitFeedback" class="summary_number"></p>
                         <p class="style_text">Awaiting <br> Feedback</p>
                     </div>
             </section>
