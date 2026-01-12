6c6b4717b5314d382fdb7243518b341c446b1f84
Schuhoch2 <githubmain@tgsweb.de>
2026-01-13 00:32:23 +0100

    fix: contact Initials now upperCase

--- scripts/get_data.js
+++ scripts/get_data.js
@@ -1,66 +1,68 @@
 //TASK
 
-async function getAddTaskData(){
+async function getAddTaskData() {
   let titleTask = document.getElementById('id_title_add_task').value;
   let descriptionTask = document.getElementById('id_description_add_task').value;
   let dueDateTask = document.getElementById('id_due_date_add_task').value;
   let priorityTask = getPriority();
   let assignedToTask = getAssignee();
   let categoryTask = getTaskCategory();
-  let subtasksTask = getAllSubtasks(); 
+  let subtasksTask = getAllSubtasks();
   let statusTask = "todo";
 
-  await switchTaskData(titleTask, descriptionTask, dueDateTask, priorityTask,assignedToTask, categoryTask, subtasksTask, statusTask); 
+  await switchTaskData(titleTask, descriptionTask, dueDateTask, priorityTask, assignedToTask, categoryTask, subtasksTask, statusTask);
 }
 
-function getPriority(){
-  return priorities.find(({name}) => {
+function getPriority() {
+  return priorities.find(({ name }) => {
     const priorityElement = document.getElementById('id_' + name + '_btn');
-    return priorityElement?.classList.contains(name +'_btn_filled' );
+    return priorityElement?.classList.contains(name + '_btn_filled');
   }) || null;
 }
 
-function getTaskCategory(){
+function getTaskCategory() {
   const categoryElement = document.getElementById('selected_category');
   let categoryContent = categoryElement.textContent;
   return categoryContent;
 }
 
-function getAllSubtasks(){
+function getAllSubtasks() {
   let subtasks = {};
   document.querySelectorAll('.list_element').forEach(li => {
     let subtaskId = crypto.randomUUID();
     let subtastText = li.querySelector('.subtask_text').textContent.trim();
-    
-    subtasks[subtaskId]= {
+
+    subtasks[subtaskId] = {
       text: subtastText,
       done: false
     };
   });
-  if (Object.keys(subtasks).length === 0){  
+  if (Object.keys(subtasks).length === 0) {
     subtasks = "";
     return subtasks;
   } else {
     return subtasks;
-  }}
+  }
+}
 
-function getAssignee(){
+function getAssignee() {
   let selectedAssignees = {};
-  document.querySelectorAll('.dropdown_item_user').forEach(listElement =>{
+  document.querySelectorAll('.dropdown_item_user').forEach(listElement => {
     const validDataChecked = listElement.querySelector('.checkbox_icon');
-    if (validDataChecked.dataset.checked === 'true'){
+    if (validDataChecked.dataset.checked === 'true') {
       const name = listElement.querySelector('.user_name_assignee_circle').textContent.trim();
       const initial = listElement.querySelector('.contact_initial_circle').textContent.trim();
       const color = listElement.querySelector('.contact_initial_circle').style.backgroundColor;
       const id = listElement.dataset.assigneeId;
-    
-      selectedAssignees [id] = {
-      assigneeName : name,
-      assigneeInitial : initial,
-      assigneeColor : color,
+
+      selectedAssignees[id] = {
+        assigneeName: name,
+        assigneeInitial: initial,
+        assigneeColor: color,
+      }
     }
-    }})
-    if (Object.keys(selectedAssignees).length === 0){  
+  })
+  if (Object.keys(selectedAssignees).length === 0) {
     selectedAssignees = "";
     return selectedAssignees;
   } else {
@@ -68,7 +70,7 @@ function getAssignee(){
   }
 }
 
-async function switchTaskData (titleTask ="", descriptionTask="", dueDateTask="", priorityTask="",assignedToTask="", categoryTask ="", subtasksTask="", statusTask=""){
+async function switchTaskData(titleTask = "", descriptionTask = "", dueDateTask = "", priorityTask = "", assignedToTask = "", categoryTask = "", subtasksTask = "", statusTask = "") {
   let taskData = {
     "title": titleTask,
     "description": descriptionTask,
@@ -79,51 +81,51 @@ async function switchTaskData (titleTask ="", descriptionTask="", dueDateTask=""
     "subtasks": subtasksTask,
     "statusTask": statusTask,
   }
- await putToStorage("tasks", taskData);
+  await putToStorage("tasks", taskData);
 }
 
 //Contacts
 
 function getElementsContacts() {
-    return {
-        name: document.getElementById('id_contact_name'),
-        email: document.getElementById('id_contact_email'),
-        phonenumber: document.getElementById('id_contact_phone'),
-    };
+  return {
+    name: document.getElementById('id_contact_name'),
+    email: document.getElementById('id_contact_email'),
+    phonenumber: document.getElementById('id_contact_phone'),
+  };
 }
 
 async function getContactData() {
-    const elements = getElementsContacts();
-    let contactName = elements.name.value;
-    let contactEmail = elements.email.value;
-    let contactPhonenumber = elements.phonenumber.value;
-    let contactColor = getRandomColor();
-    let contactInitals = getContactInitials(contactName);
+  const elements = getElementsContacts();
+  let contactName = elements.name.value;
+  let contactEmail = elements.email.value;
+  let contactPhonenumber = elements.phonenumber.value;
+  let contactColor = getRandomColor();
+  let contactInitals = getContactInitials(contactName);
 
-    await switchContactsData(contactName, contactEmail, contactColor, contactInitals, elements, contactPhonenumber,  )
+  await switchContactsData(contactName, contactEmail, contactColor, contactInitals, elements, contactPhonenumber,)
 }
 
-function getContactInitials(contactName){
-    let contactInitials = contactName.split(" ").map(word => word[0]).join("");
-    return contactInitials;
+function getContactInitials(contactName) {
+  let contactInitials = contactName.split(" ").map(word => word[0]).join("").toUpperCase();
+  return contactInitials;
 }
 
 async function switchContactsData(contactName, contactEmail, contactColor, contactInitals, elements = "", contactPhonenumber = "") {
-    let contactData = {
-        "name": contactName,
-        "eMail": contactEmail,
-        "phoneNumber": contactPhonenumber,
-        "color": contactColor,
-        "initial": contactInitals,
-    }
-    await putToStorage("contacts", contactData, elements)
+  let contactData = {
+    "name": contactName,
+    "eMail": contactEmail,
+    "phoneNumber": contactPhonenumber,
+    "color": contactColor,
+    "initial": contactInitals,
+  }
+  await putToStorage("contacts", contactData, elements)
 }
 
 //USER
 
-async function getUserData(){
+async function getUserData() {
   const elements = getElementsUser();
- 
+
   let userName = elements.name.value;
   let userEmail = elements.email.value;
   let userPassword = elements.pass.value;
